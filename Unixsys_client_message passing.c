7bc79cd86d9f2b8f607d168380d11e1476f51d96
kmseongmin <newsungk7@naver.com>
2023-12-04 18:58:44 +0900

    메세지페싱 미구현상태

--- Unixsys_client_message passing.c
+++ Unixsys_client_message passing.c
@@ -9,6 +9,17 @@
 #include <sys/stat.h>
 #include <dirent.h>
 #include <stdbool.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+#define QKEY (key_t)1230
+#define QPERM 0777
+
+typedef struct{
+    long data_type;
+    unsigned char message[1024];
+} message_entry;
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t msg_cond = PTHREAD_COND_INITIALIZER;
@@ -19,13 +30,31 @@ bool message_available = false;
 bool receive_message_available = false;
 bool new_message = true;
 
+int enter(){
+    int s_qid;
+    struct message_entry send_entry;
+    send_entry.data_type = (long)0;
+    strcpy(send_entry.message, message);
+
+    //if((s_qid = init_queue())==-1)
+        //return (-1);
+
+    if(msgsnd(s_qid,&send_entry,1024,0)==-1){
+        perror("msgsnd failed");
+        return(-1);
+    }else{
+        return (0);
+    }
+
+}
+
 void *get_message(void *arg){
 
     while(1){
         pthread_mutex_lock(&mutex);
         message = (unsigned char *)malloc(1024);
         if(receive_message_available == true){
-            printf("Receive message: %s\n",receive_message);
+            printf("Receive Server Message: %s\n",receive_message);
             free(receive_message);
             receive_message = NULL;
             receive_message_available = false;
@@ -48,7 +77,9 @@ void *send_message(void *arg){
         }   
 
         //이 부분에 ipc 기법을 이용한 message 배열을 보내기
-        //printf("Send message: %s",message);
+        int send_ok = enter();
+        if(send_ok == 0)
+            printf("Send message: %s",message);
         
         free(message);
         message = NULL;
